bd9fa511e3d73c0771f46efb509813a0bffa714c
sajjan@972308 <sajjanjasawat@gmail.com>
2023-01-17 23:09:07 +0530

    my first side

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Forgate from "./pages/Forgate";
+import Offers from "./pages/Offers";
+import Signin from "./pages/Signin"
 function App() {
   return (
     <>
-    <div>sajjan singh</div>
+    <div>sajjan singh jasawat</div>
     <Router>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="Forgate" element={<Forgate />}/>
+        <Route path="Offers" element={<Offers />}/>
+        <Route path="Signin" element={<Signin />}/>
       </Routes>
     </Router>
     </>
